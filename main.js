c83d8fd290100eeb6f9621131531e64e40c1a46b
GM_git <myemail@gmail.com>
2022-08-09 16:49:56 +0400

    change user class

--- main.js
+++ main.js
@@ -1,16 +1,9 @@
-interface User {
-    name: string;
-    id: number;
-  }
-   
-  class UserAccount {
-    name: string;
-    id: number;
-   
-    constructor(name: string, id: number) {
-      this.name = name;
-      this.id = id;
+"use strict";
+
+class UserAccount {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
     }
-  }
-   
-  const user: User = new UserAccount("Murphy", 1);
+}
+const user = new UserAccount("Murphy", 1)
